dd117c85578e2ee937aa2ab8e26372462f54b85a
manannayak19 <nayakmanan1911@gmail.com>
2025-05-17 16:40:51 -0400

    begin project fresh commit

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,18 @@
+//import { AudioWaveform } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Logo = (props: { url?: string }) => {
+  const { url = "/" } = props;
+  return (
+    <div className="flex items-center justify-center sm:justify-start">
+      <Link to={url}>
+        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+          <img src="/TPM.svg" alt="" className="size-4"/>
+          {/* <AudioWaveform className="size-4" /> */}
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
